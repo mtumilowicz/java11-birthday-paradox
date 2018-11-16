506da8b932819fde496a5ffbd15868fd8688d798
mtumilowicz <michal.tumilowicz01@gmail.com>
2018-11-16 20:04:42 +0100

    StreamDistinctChecker.java + tests

--- src/main/java/StreamDistinctChecker.java
+++ src/main/java/StreamDistinctChecker.java
@@ -3,16 +3,11 @@ import org.apache.commons.lang3.ObjectUtils;
 import java.util.HashSet;
 import java.util.stream.IntStream;
 
-import static java.util.Objects.isNull;
-
 /**
  * Created by mtumilowicz on 2018-11-16.
  */
 final class StreamDistinctChecker {
-    boolean check(IntStream ints) {
-        if (isNull(ints)) {
-            return true;
-        }
+    static boolean check(IntStream ints) {
         return ObjectUtils.defaultIfNull(ints, IntStream.empty())
                 .allMatch(new HashSet<>()::add);
     }
